5bf64c1e29dfdf2bad3a30a13604f82a47c7799e
Jazsmin Gilliam <jazzgeeter.dev@gmail.com>
2025-11-15 13:24:52 -0600

    fix: corrected a dependencies typo

--- imentor/bin/activate.fish
+++ imentor/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /Users/jazzlyn/CS/girlies-hackathon/imentor
+set -gx VIRTUAL_ENV /Users/jazzlyn/CS/girlies-hackathon/insight_mentor/imentor
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
